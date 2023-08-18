4050a07aa8cb29efb3d9c14d0f03b9ccb998b70d
lgou2w <lgou2w@hotmail.com>
2023-08-19 05:22:28 +0800

    feat: upload genshin resources

--- utilities/genshin-dict.ts
+++ utilities/genshin-dict.ts
@@ -7,16 +7,23 @@ const indexFile = path.join(dictDir, 'index.json')
 const indexFileData = await Deno.readTextFile(indexFile)
 const mappings = JSON.parse(indexFileData) as Record<string, Record<string, string>>
 
-// Load ids.json
-const idsFile = path.join(dictDir, 'ids.json')
-const idsFileData = await Deno.readTextFile(idsFile)
-const ids = JSON.parse(idsFileData) as Record<string, string>
+/*
+// FIXME: Deprecated, as official resources are no longer used.
+//
+// // Load ids.json
+// const idsFile = path.join(dictDir, 'ids.json')
+// const idsFileData = await Deno.readTextFile(idsFile)
+// const ids = JSON.parse(idsFileData) as Record<string, string>
 
-// Category -> miHoYo Category
-const categoryMappings: Record<string, string> = {
-  character: 'character_icon',
-  weapon: 'equip'
-}
+// // Category -> miHoYo Category
+// const categoryMappings: Record<string, string> = {
+//   character: 'character_icon',
+//   weapon: 'equip'
+// }
+//
+// https://upload-bbs.mihoyo.com/game_record/genshin/${miHoYoCategory}/${idToName}.png
+//
+*/
 
 export function handleRequest (
   req: Request,
@@ -27,18 +34,14 @@ export function handleRequest (
 
   const [, category, name] = execArray
   const decodedName = decodeURIComponent(name)
-  const itemId = mappings[category]?.[decodedName]
-  if (!itemId) return false
-
-  const idToName = ids[itemId]
-  const miHoYoCategory = categoryMappings[category]
-  if (!idToName || !miHoYoCategory) return false
+  const identifier = mappings[category]?.[decodedName]
+  if (!identifier) return false
 
   return new Response(null, {
     status: 302,
     headers: {
       ...req.headers,
-      'location': `https://upload-bbs.mihoyo.com/game_record/genshin/${miHoYoCategory}/${idToName}.png`
+      'location': `/static/genshin/${category}/${identifier}.png`
     }
   })
 }
